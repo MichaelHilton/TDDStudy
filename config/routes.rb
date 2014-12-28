003a00b7e094e42d2a015262b3ad82e390588f58
Michael Hilton <michael.hilton@gmail.com>
2014-12-27 21:06:18 -0800

    move files to /markup from /viz

--- config/routes.rb
+++ config/routes.rb
@@ -34,6 +34,8 @@ Rails.application.routes.draw do
 
   get 'markup/retrieve_session' => 'markup#retrieve_session'
 
+  get 'markup/markupComparison' => 'markup#markupComparison'
+
   get 'markup/:researcher' => 'markup#researcher'
 
 
